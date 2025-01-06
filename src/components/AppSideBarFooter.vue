bf6e5bee6b1ac88cd2aa9fd9209c44797300ad80
JK117 <sheppard.jk@outlook.com>
2025-01-06 16:32:23 +0800

    :sparkles: Better Side Bar Footer

--- src/components/AppSideBarFooter.vue
+++ src/components/AppSideBarFooter.vue
@@ -5,12 +5,24 @@ const siteFilingLink = ref('https://beian.miit.gov.cn/')
 </script>
 
 <template>
-  <a-button 
-    :href="siteFilingLink" 
-    target="_blank" 
-    type="link" 
-    size="small"
-  >
-    {{ siteFilingNum }}
-  </a-button>
+    <a-flex
+      vertical 
+      justify="flex-end" 
+      align="center" 
+      style="width: 100%;height: 100%;"
+    >
+      <a-button 
+        :href="siteFilingLink" 
+        target="_blank" 
+        type="link" 
+        size="small" 
+        style="max-width: 100%;"
+      >
+        <div style="overflow: hidden;text-overflow: ellipsis;">
+          <span>
+            {{ siteFilingNum }}
+          </span>
+        </div>
+      </a-button>
+    </a-flex>
 </template>
